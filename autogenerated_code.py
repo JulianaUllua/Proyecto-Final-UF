720248069be1b577b761824278cdd9ef9004f5ce
t.monreal <trinidadmonreal@gmail.com>
2021-09-29 23:57:47 -0300

    extract_code

--- autogenerated_code.py
+++ autogenerated_code.py
@@ -2,3 +2,9 @@ import numpy as np
 from cv2 import cv2
 from matplotlib import pyplot as plt
 
+
+filename_0 = r'C:\Users\trini\Pictures\lena.png'
+img_0 = cv2.imread(filename_0)
+filename_1 = r'C:\Users\trini\Pictures\lena.png'
+img_1 = cv2.imread(filename_1)
+img_2 = cv2.bitwise_and(img_1, img_0,None)
